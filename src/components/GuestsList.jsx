08d8a1c5182985bc58ceddef6f690e280c78e5f2
MTristan <53493044+MTristan012@users.noreply.github.com>
2023-05-12 20:13:09 -0600

    A4.2

--- src/components/GuestsList.jsx
+++ src/components/GuestsList.jsx
@@ -1,8 +1,10 @@
+import Button from "react-bootstrap/Button";
+
 export default function GuestsList() {
-    let child
-    let adult
-    let setAdult
-    let setChild
+  let child;
+  let adult;
+  let setAdult;
+  let setChild;
 
   function incrementAdult() {
     if (child < 10 - adult) {
@@ -25,30 +27,100 @@ export default function GuestsList() {
   }
   return (
     <>
+      <br />
       <div>
         <h6>Adults</h6>
-        <h6>Age 13 or above</h6>
+        <h6 style={{ color: "#cecece" }} className="mb-3">
+          Age 13 or above
+        </h6>
         <div>
-          <button onClick={decrementAdult} disabled={adult <= 0}>
-            -
-          </button>
+          <Button
+            variant="outline-dark"
+            onClick={decrementAdult}
+            disabled={adult <= 0}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-dash-lg"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"
+              />
+            </svg>
+          </Button>
           <span>{adult}</span>
-          <button onClick={incrementAdult} disabled={adult >= 10}>
-            +
-          </button>
+          <Button
+            variant="outline-dark"
+            onClick={incrementAdult}
+            disabled={adult >= 10}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-plus-lg"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+              />
+            </svg>
+          </Button>
         </div>
       </div>
+      <br />
       <div>
         <h6>Children</h6>
-        <h6>Age 2 - 12</h6>
+        <h6 style={{ color: "#cecece" }} className="mb-3">
+          Age 2 - 12
+        </h6>
         <div>
-          <button onClick={decrementChild} disabled={child <= 0}>
-            -
-          </button>
+          <Button
+            variant="outline-dark"
+            onClick={decrementChild}
+            disabled={child <= 0}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-dash-lg"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8Z"
+              />
+            </svg>
+          </Button>
           <span>{child}</span>
-          <button onClick={incrementChild} disabled={child >= 10}>
-            +
-          </button>
+          <Button
+            variant="outline-dark"
+            onClick={incrementChild}
+            disabled={child >= 10}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-plus-lg"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"
+              />
+            </svg>
+          </Button>
         </div>
       </div>
     </>
